6d9cd89484911d6eaee542c45f1ca6613b9cf121
Ravindra <rramcha@users.noreply.github.com>
2020-09-13 02:13:16 -0400

    QA

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and testing.
+          Edit <code>src/App.js</code> and save to reload and testing and QA.
         </p>
         <a
           className="App-link"
